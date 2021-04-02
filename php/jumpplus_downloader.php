643fa97d14826a86b6129a69fd31525c1c49fe59
fa0311 <yukinashi2742756@yahoo.co.jp>
2021-04-03 04:20:41 +0900

    hard

--- php/jumpplus_downloader.php
+++ php/jumpplus_downloader.php
@@ -7,8 +7,6 @@ class jumppuls_downloader
         $this->file = 0;
         $this->h = 1200;
         $this->w = 760;
-        $this->h_marge = 4;
-        $this->w_marge = 6;
     }
 
 
@@ -24,8 +22,6 @@ class jumppuls_downloader
             if ($page["type"] == "main") {
                 $this->h = $page["height"];
                 $this->w = $page["width"];
-                if ($page["width"] == 764)
-                    $this->w_marge = 7;
                 $this->download($page["src"]);
                 $this->processing();
                 $this->output("./" . $this->list["readableProduct"]["title"] . "/");
@@ -49,9 +45,9 @@ class jumppuls_downloader
 
     public function processing()
     {
-        $this->dst_image = imagecreatetruecolor($this->w - $this->w_marge * 4, $this->h - $this->h_marge * 4);
-        $width = $this->w / 4 - $this->w_marge;
-        $height = $this->h / 4 - $this->h_marge;
+        $this->dst_image = imagecreatetruecolor($this->w - 6 * 4, $this->h - 4 * 4);
+        $width = $this->w / 4 - 6;
+        $height = $this->h / 4 - 4;
         for ($i = 0; $i <= 3; $i++) {
             for ($ii = 0; $ii <= 3; $ii++) {
                 imagecopyresampled($this->dst_image, $this->img, $width * $i, $height * $ii, $width * $ii, $height * $i, $width, $height, $width, $height);
