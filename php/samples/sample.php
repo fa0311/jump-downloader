97b49acb224563c725ba436fce2fa237b15d7673
fa0311 <yukinashi2742756@yahoo.co.jp>
2021-04-03 04:11:35 +0900

    ずれるバグ修正

--- php/samples/sample.php
+++ php/samples/sample.php
@@ -1,5 +1,9 @@
 <?php
 set_time_limit(0);
-include('jumppuls_downloader.php');
+include('jumpplus_downloader.php');
 $instance = new jumppuls_downloader();
+/*ずれる場合は以下を各自で値を調整して指定してください
+$instance->h_marge = 4;
+$instance->w_marge = 6;
+*/
 $instance->auto_list_download("https://shonenjumpplus.com/episode/10833519556325021865", true);
